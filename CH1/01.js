5a686c9d571cfdfcd2eddd710bbb92d49bcec8ee
gene <jj.gene0516@gmail.com>
2024-09-09 21:16:35 +0900

    ch1: 함수 추출하기

--- CH1/01.js
+++ CH1/01.js
@@ -1,46 +1,57 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `청구 내역( 고객명 : ${invoice.customer})\n`;
+  let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
+
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-    minimumSignificantDigits: 2,
+    minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+  for (let perf of invoice[0].performances) {
+    let thisAmount = amountFor(perf, playFor(perf)); // 추출한 함수 활용
 
-    switch (play.type) {
-      case "tragedy": // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
-
+    // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트 제공
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
-    // 청구 내역 출력
-    result += `${play.name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
-    }석)\n`;
+    }석\n)`;
     totalAmount += thisAmount;
   }
-
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  // 임시변수를 질의변수로 바꾸기
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  // 중첩함수
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
 }
