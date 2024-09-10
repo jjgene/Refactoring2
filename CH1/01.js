bb1ecd8d6110c7a851cfd70c61224a2076c0863b
gene <jj.gene0516@gmail.com>
2024-09-10 11:22:28 +0900

    명확한 이름으로 변경

--- CH1/01.js
+++ CH1/01.js
@@ -1,18 +1,18 @@
-function amountFor(perf, play) {
-  let thisAmount = 0;
+function amountFor(aPerformance, play) {
+  let result = 0;
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
@@ -31,17 +31,20 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice[0].performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+  for (let aPerformance of invoice[0].aPerformanceormances) {
+    const play = plays[aPerformance.playID];
+    let result = amountFor(aPerformance, play);
 
     // 포인트 적립
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트 제공
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === play.type)
+      volumeCredits += Math.floor(aPerformance.audience / 5);
     //청구 내역을 출력한다.
-    result += `${play.name}:${format(thisAmount / 100)}(${perf.audience}석\n)`;
-    totalAmount += thisAmount;
+    result += `${play.name}:${format(result / 100)}(${
+      aPerformance.audience
+    }석\n)`;
+    totalAmount += result;
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
