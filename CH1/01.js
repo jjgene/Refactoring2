c06a93d5cc8d7811671b4c8ac03a538c9403de20
gene <jj.gene0516@gmail.com>
2024-09-10 10:48:00 +0900

    ch1:statement

--- CH1/01.js
+++ CH1/01.js
@@ -12,7 +12,26 @@ function statement(invoice, plays) {
 
   for (let perf of invoice[0].performances) {
     const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play); // 추출한 함수 활용
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+    let thisAmount = amountFor(perf, play);
 
     // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -27,27 +46,4 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
-
-  // 중첩함수
-  function amountFor(aPerformance, play) {
-    let result = 0;
-    switch (play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    return result;
-  }
 }
