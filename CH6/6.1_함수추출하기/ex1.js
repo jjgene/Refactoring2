5451601bf4c2964253c1a32f4b17d81f9694e374
gene <jj.gene0516@gmail.com>
2024-09-13 14:03:53 +0900

    ch6:유효범위를 벗어나지않는경우

--- CH6/6.1_함수추출하기/ex1.js
+++ CH6/6.1_함수추출하기/ex1.js
@@ -1,9 +1,6 @@
 function printOwing(invoice) {
   let outstanding = 0;
-  console.log("************");
-  console.log("***고객채무***");
-  console.log("************");
-
+  printBanner(); //
   //미해결 채무(outstanding) 를 계산한다.
   for (const o of invoice.orders) {
     outstanding += o.amount;
@@ -16,8 +13,16 @@ function printOwing(invoice) {
     today.getDate() + 30
   );
   //세부 사항을 출력한다.
+  printDetails();
 
-  console.log(`고객명 : ${invoice.customer}`);
-  console.log(`채무액 : ${outstanding}`);
-  console.log(`마감일 : ${invoice.dueDate.toLocaleDateString}`);
+  function printBanner() {
+    console.log("************");
+    console.log("***고객채무***");
+    console.log("************");
+  }
+  function printDetails() {
+    console.log(`고객명 : ${invoice.customer}`);
+    console.log(`채무액 : ${outstanding}`);
+    console.log(`마감일 : ${invoice.dueDate.toLocaleDateString}`);
+  }
 }
